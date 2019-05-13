bc3ff3d22928d0bb5bdf102394e4dc7b4ed75992
matt_g <mgaut@uci.edu>
2019-05-12 17:38:52 -0700

    Added JumpItemEffect

---
+++ SSAmusement/Assets/Items/AddJumpItemEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddJumpItemEffect : ItemEffect {
+    [SerializeField] int jumps;
+
+    public override void OnDrop(Item item) {
+        item.owner.RemoveBonusJump(jumps);
+    }
+
+    public override void OnPickup(Item item) {
+        item.owner.AddBonusJump(jumps);
+    }
+}
