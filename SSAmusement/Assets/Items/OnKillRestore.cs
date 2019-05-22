fc50a8890771691e08cc72a91c343ca299eea72e
matt_g <mgaut@uci.edu>
2019-05-22 02:43:30 -0700

    Created PercussiveCapacitor Item

---
+++ SSAmusement/Assets/Items/OnKillRestore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKillRestore : OnKillItemEffect {
+
+    [SerializeField] float health_to_restore, energy_to_restore;
+
+    protected override void OnKill(Character c, Character killed) {
+        if (energy_to_restore > 0) c.RestoreEnergy(energy_to_restore);
+        if (health_to_restore > 0) c.RestoreHealth(health_to_restore);
+    }
+}
