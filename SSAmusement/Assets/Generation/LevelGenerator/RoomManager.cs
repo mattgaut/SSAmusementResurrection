0191a1ab976ccf02c511b52f838ed8f608534e72
matt_g <mgaut@uci.edu>
2019-06-27 14:54:11 -0700

    Created New Item Effects

--- SSAmusement/Assets/Generation/LevelGenerator/RoomManager.cs
+++ SSAmusement/Assets/Generation/LevelGenerator/RoomManager.cs
@@ -6,11 +6,17 @@ public class RoomManager : Singleton<RoomManager> {
 
     public RoomController active { get; private set; }
 
+    public delegate void OnEnterRoom(RoomController entered, bool is_first_time);
+
+    public event OnEnterRoom on_enter_room;
+
     [SerializeField] SpriteRenderer background_prefab;
     [SerializeField] List<GameObject> objects_to_center_on_active_room;
     List<GameObject> loaded_backgrounds;
     SpriteRenderer main_background;
     Dictionary<Room, List<Room>> rooms;
+
+    HashSet<RoomController> discovered_rooms;
 
     public void LoadBackgrounds(Sprite background) {
         loaded_backgrounds = new List<GameObject>();
@@ -23,6 +29,7 @@ public class RoomManager : Singleton<RoomManager> {
 
     public void SetRooms(Dictionary<Room, List<Room>> rooms) {
         this.rooms = rooms;
+        discovered_rooms = new HashSet<RoomController>();
     }
 
     public void SetActiveRoom(RoomController room_controller, bool set_focus = true) {
@@ -37,6 +44,9 @@ public class RoomManager : Singleton<RoomManager> {
                 foreach (GameObject go in loaded_backgrounds) {
                     go.transform.position = active.transform.position + new Vector3(active.room.size.x * Room.Section.width, active.room.size.y * Room.Section.height, 0)/2f;
                 }
+
+                on_enter_room?.Invoke(active, discovered_rooms.Contains(active));
+                discovered_rooms.Add(active);
             }
         }
     }
---
+++ SSAmusement/Assets/Items/OnEnterRoomItemEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class OnEnterRoomItemEffect : ItemEffect {
+
+    [SerializeField] bool is_first_enter_only;
+
+    protected override void OnDrop() {
+        RoomManager.instance.on_enter_room += OnEnterRoom;
+    }
+
+    protected override void OnPickup() {
+        RoomManager.instance.on_enter_room -= OnEnterRoom;
+    }
+
+    protected void OnEnterRoom(RoomController room, bool is_first_enter) {
+        if (is_first_enter_only && !is_first_enter) {
+            return;
+        }
+        OnEnterRoomTrigger(room);
+    }
+
+    protected abstract void OnEnterRoomTrigger(RoomController room);
+}
