d962a102d61234a87c016f7c0c4dd0288ccfce6f
matt_g <mgaut@uci.edu>
2019-06-29 19:30:30 -0700

    Created Cybernetic Repair Item

---
+++ SSAmusement/Assets/Items/OnKillDropPickupsItemEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKillDropPickupsItemEffect : OnKillItemEffect {
+
+    [SerializeField] int enemies_per_drop;
+    [SerializeField] Pickup to_drop;
+
+    int count;
+
+    protected override void OnKill(Character killer, Character killed) {
+        count++;
+        if (count >= enemies_per_drop) {
+            killed.DropObject(to_drop.gameObject, true);
+            count = 0;
+        }
+    }
+}
