055012ee4b7b08e0d589540ce70ed97b7abd51e2
matt_g <mgaut@uci.edu>
2019-07-04 23:58:49 -0700

    Created Attack speed buff

---
+++ SSAmusement/Assets/Buffs/AttackSpeedBuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpeedBuff : BuffDefinition<StatBuffInfo> {
+
+    [SerializeField] protected Stat.Modifier modifier;
+
+    public override BuffType type {
+        get { return BuffType.stat; }
+    }
+
+    protected override void ApplyEffects(Character character, StatBuffInfo info, IBuff buff) {
+        character.stats.attack_speed.AddModifier(modifier);
+    }
+
+    protected override StatBuffInfo GetBuffInfo(IBuff buff) {
+        return new StatBuffInfo(buff, new Stat.Modifier(modifier));
+    }
+
+    protected override void RecalculateEffects(StatBuffInfo info, IBuff buff) {
+        info.modifier.Set(modifier * buff.stack_count);
+    }
+    protected override void RemoveEffects(Character character, StatBuffInfo info) {
+        character.stats.attack_speed.RemoveModifier(modifier);
+    }
+}
