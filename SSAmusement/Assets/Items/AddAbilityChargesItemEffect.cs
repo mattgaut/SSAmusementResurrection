d1de9f237cfa22912a5be604d8b8096962856303
matt_g <mgaut@uci.edu>
2019-07-11 16:26:31 -0700

    Added Charge Granting Item Effect

---
+++ SSAmusement/Assets/Items/AddAbilityChargesItemEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddAbilityChargesItemEffect : ItemEffect {
+    [SerializeField] int charges;
+    [SerializeField] List<int> abilities;
+
+    protected override void OnDrop() {
+        foreach (int i in abilities) {
+            item.owner.abilities.GetAbility(i)?.active_cooldown?.ModifyBonusCharges(-charges);
+        }
+    }
+
+    protected override void OnPickup() {
+        foreach (int i in abilities) {
+            item.owner.abilities.GetAbility(i)?.active_cooldown?.ModifyBonusCharges(charges);
+        }
+    }
+}
