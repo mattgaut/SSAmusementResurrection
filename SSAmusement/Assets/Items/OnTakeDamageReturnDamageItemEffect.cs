7b72fcd8c4d1aecf82eb5f3f5dad7695b331da8a
matt_g <mgaut@uci.edu>
2019-07-18 03:32:58 -0700

    Created Return damage item effect

---
+++ SSAmusement/Assets/Items/OnTakeDamageReturnDamageItemEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTakeDamageReturnDamageItemEffect : OnTakeDamageItemEffect {
+    [SerializeField] bool is_pre_mitigation_damage;
+    [SerializeField] float damage_multtiplier;
+    [SerializeField] float flat_damage;
+
+    protected override void OnTakeDamage(Character hit, float pre_damage, float post_damage, Character source) {
+        hit.DealDamage(((is_pre_mitigation_damage ? pre_damage : post_damage) * damage_multtiplier * item.stack_count) + flat_damage, source, false);
+    }
+}
