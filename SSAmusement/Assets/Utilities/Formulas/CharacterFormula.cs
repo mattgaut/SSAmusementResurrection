f53a59eb77c7ea11fc1d5777c8e56f9c9cbcbf49
matt_g <mgaut@uci.edu>
2019-07-19 05:02:50 -0700

    Created Formulas

---
+++ SSAmusement/Assets/Utilities/Formulas/CharacterFormula.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFormula : Formulas.Formula<Character> {
+    [SerializeField] 
+
+    public override float GetValue(Character t1) {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ SSAmusement/Assets/Utilities/Formulas/Formula.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Formulas {
+    public abstract class Formula : ScriptableObject {
+        public abstract float GetValue();
+    }
+
+    public abstract class Formula<T1> : ScriptableObject {
+        public abstract float GetValue(T1 t1);
+    }
+
+    public abstract class Formula<T1, T2> : ScriptableObject {
+        public abstract float GetValue(T1 t1, T2 t2);
+    }
+
+    public abstract class Formula<T1, T2, T3> : ScriptableObject {
+        public abstract float GetValue(T1 t1, T2 t2, T3 t3);
+    }
+}
